588a33b09f009ff2a31745952815320478038da6
Patricia Faustino <patriciafaustinobr@gmail.com>
2022-06-13 22:37:56 -0300

    create sidebar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,24 @@
-import { Header } from "./components/Header";
+import { Header } from "./components/Header/Header";
+import { Sidebar } from "./components/Sidebar/Sidebar";
 import { Post } from "./Post";
-import './global.css';
+import "./global.css";
+
+import styles from "./App.module.css";
 
 function App() {
   return (
     <div>
       <Header />
-      <Post
-        author="Patricia Faustino"
-        content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-      />
-      <Post
-        author="Fernandinha Silva"
-        content="Dia bacana, dia legal"
-      />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Patricia Faustino"
+            content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+          />
+          <Post author="Fernandinha Silva" content="Dia bacana, dia legal" />
+        </main>
+      </div>
     </div>
   );
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,10 @@
 import styles from "./Header.module.css";
-import igniteLogo from "../assets/ignite-logo.svg";
+import igniteLogo from "../../assets/ignite-logo.svg";
 
 export function Header() {
   return (
     <header className={styles.header}>
-      <img src={igniteLogo} alt="Ignite Logo"/>
+      <img src={igniteLogo} alt="Ignite Logo" />
       <strong className="header">Ignite Feed</strong>
     </header>
   );
