091f68e66a6f0d9a4ebd772497b72417a3203b1e
Patricia Faustino <patriciafaustinobr@gmail.com>
2022-06-14 22:58:00 -0300

    componentizing images

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,3 +1,4 @@
+import { Avatar } from "./Avatar";
 import { Comment } from "./Comment";
 import styles from "./Post.module.css";
 
@@ -7,10 +8,7 @@ export function Post() {
       <header>
         <div className={styles.author}>
           <div>
-            <img
-              className={styles.avatar}
-              src="https://avatars.githubusercontent.com/u/69044157?v=4"
-            />
+            <Avatar src="https://avatars.githubusercontent.com/u/69044157?v=4" />
           </div>
           <div className={styles.authorInfo}>
             <strong>Patricia Faustino</strong>
