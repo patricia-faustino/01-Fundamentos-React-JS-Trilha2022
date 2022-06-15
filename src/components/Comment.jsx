42a3542559fa6f935b14bf6dcbf1c4e15f75e782
Patricia Faustino <patriciafaustinobr@gmail.com>
2022-06-14 22:37:05 -0300

    adding Comment component

---
+++ src/components/Comment.jsx
@@ -0,0 +1,32 @@
+import { ThumbsUp, Trash } from "phosphor-react";
+import styles from "./Comment.module.css";
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img src="https://avatars.githubusercontent.com/u/69044157?v=4" />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Patricia Faustino</strong>
+              <time dateTime="11 de Maio às 08:13h">Cerca de 2h atrás</time>
+            </div>
+
+            <button title="Deletar comentário">
+              <Trash size={24} />
+            </button>
+          </header>
+          <p>Muito bom Devon, parabéns!! 👏👏</p>
+        </div>
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+}
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,3 +1,4 @@
+import { Comment } from "./Comment";
 import styles from "./Post.module.css";
 
 export function Post() {
@@ -24,12 +25,28 @@ export function Post() {
           Acabei de subir mais um projeto no meu portifa. É um projeto que fiz
           no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀
         </p>
-        <p>👉{' '}<a href="">jane.design/doctorcare</a></p>
-        <p> 
-            <a href="">#novoprojeto</a>{' '}
-            <a href=""> #nlw</a>{' '}
-            <a href=""> #rocketseat</a>
+        <p>
+          👉 <a href="">jane.design/doctorcare</a>
         </p>
+        <p>
+          <a href="">#novoprojeto</a> <a href=""> #nlw</a>{" "}
+          <a href=""> #rocketseat</a>
+        </p>
+      </div>
+
+      <form className={styles.commentForm}>
+        <strong>Deixe seu feedback</strong>
+        <textarea placeholder="Deixe um comentário" />
+        <footer>
+          <button type="submit">Publicar</button>
+        </footer>
+      </form>
+
+      <div className={styles.commentList}>
+        <Comment />
+        <Comment />
+        <Comment />
+        <Comment />
       </div>
     </article>
   );
